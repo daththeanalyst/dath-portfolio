4a152ae23f6f5cfbb944b956fba533ad16cb2b47
daththeanalyst <daththeanalyst@users.noreply.github.com>
2026-03-11 21:51:54 +0000

    deploy: 9ec0dd38afda7bdc6eb2275c0347666acc86c92a

---
+++ _next/static/9KH8EJHlCRN069AuKeSwb/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dath-portfolio//_next/:path+",
+        "destination": "/dath-portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
